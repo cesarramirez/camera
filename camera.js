36994b57ccd8290283165ad43bf37d0e404795bb
Tim Park <timfpark@gmail.com>
2013-09-27 20:03:50 -0700

    simplify code

--- camera.js
+++ camera.js
@@ -10,9 +10,9 @@ async.each(config.cameras, function(camera, callback) {
         if (err) { return console.log('failed to connect camera: ' + err); }
 
 	// startup opencv based camera manager (supports software based motion detection) for this camera. 
-
-        var cameraManager = new OpenCVCameraManager(camera);
-        cameraManager.start(session, { $or: [ { to: camera.id }, { from: camera.id } ] }, function(err) {});
+        new OpenCVCameraManager(camera).start(session, { $or: [ { to: camera.id }, { from: camera.id } ] }, function(err, message) {
+            if (err) return session.log.error(JSON.stringify(err));
+        });
 
         callback();
     });
