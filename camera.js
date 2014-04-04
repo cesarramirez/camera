b42d8a5a99195f4d7e8296e1e7444b6e5e49f2d1
Tim Park <timfpark@gmail.com>
2014-04-04 14:45:20 -0700

    update camera sample to latest

--- camera.js
+++ camera.js
@@ -1,6 +1,6 @@
 var config = require('./config')
   , nitrogen = require('nitrogen')
-//  , ImageSnapCamera = require('nitrogen-imagesnap')
+  , ImageSnapCamera = require('nitrogen-imagesnap')
   , FSWebcamCamera = require('fswebcam')
   , CameraManager = require('nitrogen-camera');
 
@@ -9,14 +9,12 @@ var service = new nitrogen.Service(config);
 var camera = new FSWebcamCamera({
     nickname: 'camera',
     name: "Camera"
-})
+});
 
-service.connect(config.camera, function(err, session, camera) {
+service.connect(camera, function(err, session, camera) {
     if (err) { return console.log('failed to connect camera: ' + err); }
 
     new CameraManager(camera).start(session, function(err, message) {
         if (err) return session.log.error(JSON.stringify(err));
     });
-
-    return callback();
 });
